5e8f9b5cd1a5e3241dfc778616dad3f0758dc099
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-06-06 12:11:52 +0530

    Time: 20 ms (39.31%) | Memory: 26.7 MB (70.54%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       map <int , int >mpp;
-       int n = nums.size();
 
-        for (int i =0;i<n;i++){
-            mpp[nums[i]]++;
-        }
+        // MOORES VOTING ALGORITHM
 
-        for (auto it : mpp){
-            if (it.second > (n/2)){
-                return it.first ;
+        int cnt =0;
+        int el ;
+
+        for (int i =0;i<nums.size();i++){
+            if (cnt ==0){
+                el = nums[i];
+                cnt++;
+            }
+            else if(nums[i]== el){
+                cnt ++;
+            }
+
+            else if(nums[i] != el){
+                cnt --;
             }
         }
-        return -1;       
+
+        int cnt1 =0;
+
+        for (int i =0;i<nums.size();i++){
+            if (nums[i]== el){
+                cnt1++;
+            }
+
+        }
+        if (cnt1>nums.size()/2){
+            return el ;
+        }
+
+        return -1;
+
+        
+        
     }
 };
