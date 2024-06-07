1a212564ae73d6fce94c55c7483a0acd2c587919
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-06-07 12:59:15 +0530

    Time: 74 ms (43.15%) | Memory: 29 MB (55.68%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        set<vector<int>> st;  // Declare the set to store unique triplets
-        int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;  // Skip duplicates
-            }
-
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
+        for (int i =0;i<nums.size();i++){
+            if (i>0 && nums[i]== nums[i-1]) continue;
+            int j =i+1;
+            int k = nums.size()-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if (sum<0){
                     j++;
-                } else if (sum > 0) {
+                }
+                else if (sum>0){
                     k--;
-                } else {
+                }
+                else{
                     vector<int> temp = {nums[i], nums[j], nums[k]};
-                    st.insert(temp);  // Insert the triplet into the set
+                    ans.push_back(temp);
                     j++;
                     k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
-
-        vector<vector<int>> ans(st.begin(), st.end());  // Convert the set to a vector
-        return ans;
+    return ans;
     }
+    
 };
