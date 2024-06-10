5018454662197e94aa2ff300093a625e438de893
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-06-10 20:48:18 +0530

    Time: 31 ms (15.50%) | Memory: 15.7 MB (13.36%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+
+        if (s.size()==0){
+            return 0;
+        }
+        
+        int ans = INT_MIN;
+        set<int> st;
+
+        int l =0;
+        int r =0;
+
+        for(int r =0;r<s.size();r++){
+            if(st.find(s[r])!=st.end()){
+                while(l<r && st.find(s[r])!=st.end()){
+                    st.erase(s[l]);
+                    l++;
+                }
+
+            }
+            st.insert(s[r]);
+            ans = max(ans, r-l+1);
+        }
+        return ans;
+    }
+};
