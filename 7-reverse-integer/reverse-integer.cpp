984339da6845f02a244456869b2934635244bd24
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-06-29 20:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (52.26%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long rev = 0;
+        int digit ;
+
+        while(x!=0){
+            digit = x%10;
+            rev = rev * 10 + digit;
+            x= x/10;
+        }
+
+        if(rev> INT_MAX || rev< INT_MIN){
+            return 0;
+        }
+        return rev;
+    }
+};
