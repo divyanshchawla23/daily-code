899bc91c73eca8f3cddd596c89d8aff91e2fd2ce
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-07-01 19:54:42 +0530

    Time: 7 ms (55.05%) | Memory: 14.3 MB (80.41%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,51 @@
+class mycom
+ {
+     public:
+     bool operator()(pair<int,int> const &p1,pair<int,int>p2 )
+     {
+         if(p1.first==p2.first)
+         {
+             return p2.second>p1.second;
+         }
+         return p1.first>p2.first;
+     }
+ };
+
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+
+        vector <int> ans ;
+
+        map <int , int > mpp;
+
+        priority_queue< pair<int, int>, 
+        vector <pair<int , int >>, 
+        mycom> hp;
+
+
+        for(int i =0;i<nums.size();i++){
+            mpp[nums[i]] += 1;
+        }
+
+        for(auto it : mpp){
+            hp.push({it.second , it.first});
+
+        }
+
+        while(hp.size()>0){
+            int freq= hp.top().first;
+            int el = hp.top().second;
+            for(int i =1;i<=freq;i++){
+                ans.push_back(el);
+            }
+            hp.pop();
+        }
+
+      //  sort(ans.begin(), ans.end());
+
+        return ans ;
+        
+    }
+};
