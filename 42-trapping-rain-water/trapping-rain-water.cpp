4c00ab27dba7fe3a537fdab8ec221590cd5e995f
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-07-07 02:13:14 +0530

    Time: 3 ms (98.96%) | Memory: 23.2 MB (15.08%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& nums) {
+       int n =  nums.size();
+
+       vector<int> prefix(n);
+       vector<int> suffix(n);
+
+       prefix[0]= nums[0];
+
+       for(int i =1;i<n;i++){
+    
+        prefix[i]= max(prefix[i-1],nums[i]);
+       }
+
+        suffix[n-1]=nums[n-1];
+       for(int i =n-2;i>=0;i--){
+       
+        suffix[i]= max(suffix[i+1],nums[i]);
+       }
+
+       long long  water =0;
+       for(int i =0;i<n;i++){
+        water+= min(prefix[i],suffix[i])- nums[i];
+       }
+
+       return water;
+       
+    }
+};
