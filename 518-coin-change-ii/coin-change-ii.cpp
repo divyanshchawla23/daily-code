a418aea0c9f3da06ed953196ceb2e2e9331923ee
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-07-30 16:32:37 +0530

    Time: 11 ms (54.88%) | Memory: 13.5 MB (79.76%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int helper(vector<int> & coins , int sum, int n){
+        int dp[n+1][sum+1];
+        for(int i =0;i<n+1;i++){
+            for(int j=0 ;j<sum+1;j++){
+                if(i==0) dp[i][j] = 0;
+                if(j==0) dp[i][j] = 1;
+            }
+
+        }
+        for(int i =1;i<n+1;i++){
+            for(int j=1 ;j<sum+1;j++){
+                
+                if(j>=coins[i-1]){
+                    dp[i][j]= dp[i][j-coins[i-1]] + dp[i-1][j];
+
+                }
+                else
+                dp[i][j]=  dp[i-1][j];
+            }
+
+        }
+
+        return dp[n][sum];
+
+
+
+    }
+    
+    int change(int amount, vector<int>& coins) {
+       int n = coins.size();
+        return helper(coins , amount, n); 
+    }
+};
