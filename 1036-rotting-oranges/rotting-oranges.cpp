32f680f31fefb73525aa51baf7993d21b9a84bf8
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-08-05 06:53:14 +0530

    Time: 5 ms (57.96%) | Memory: 16.4 MB (34.29%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -39,7 +39,7 @@ public:
 
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(ans[i][j]!=2 && grid[i][j]==1) return -1;
+                if(ans[i][j]==1) return -1;
                 
             }
         }
