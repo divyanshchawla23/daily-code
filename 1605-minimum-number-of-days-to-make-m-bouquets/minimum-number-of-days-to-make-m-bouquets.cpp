94d2069fdc2c258b09350ab202d6e7fd032a5cc1
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-08-21 16:22:21 +0530

    Time: 118 ms (32.89%) | Memory: 69.3 MB (30.18%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,32 +1,25 @@
 class Solution {
-public:
+private:
 
-    bool possible(vector<int>& nums,int mid, int m, int k){
-
+    bool isPossible(vector<int>& nums,int mid, int m, int k){
         int cnt =0;
-        int numberB=0;
-
+        int number=0;
         for(int i =0;i<nums.size();i++){
-            if (nums[i]<=mid){
-                cnt = cnt +1;
-            }
-            else {
-               numberB+= (cnt/k);
-               cnt = 0; 
+            if(nums[i]<=mid) cnt++;
+            else{
+                number += cnt/k;
+                cnt=0;
             }
         }
-        numberB+= (cnt/k);
+        number += cnt/k;
 
-        if(numberB>=m) return true;
-        else{return false;}
-
+        if(number>=m) return true;
+        else return false;
     }
-
+public:
     int minDays(vector<int>& nums, int m, int k) {
         long long val = m *1LL * k * 1LL;
         if(val>nums.size()) return -1 ;
-
-        
 
         int low = *min_element(nums.begin(), nums.end());
         int high = *max_element(nums.begin(), nums.end());
@@ -35,7 +28,7 @@ public:
         while(low<=high){
             int mid = low + (high-low)/2;
 
-            if (possible(nums, mid,  m,  k)){
+            if (isPossible(nums, mid,  m,  k)){
                 ans = mid;
                 high = mid - 1;
             }
@@ -45,8 +38,5 @@ public:
         }
 
         return ans ;
-
-        
-        
     }
 };
