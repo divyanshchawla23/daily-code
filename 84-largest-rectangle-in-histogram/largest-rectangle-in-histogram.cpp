64d5ad5e5371d93482796ec5c94e2027331efa46
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-08-26 23:57:37 +0530

    Time: 162 ms (7.33%) | Memory: 103.1 MB (5.89%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,83 +1,62 @@
 class Solution {
+private:
+    vector<int> findLeft (vector<int>& nums){
+        stack<pair<int,int>> st;
+        vector<int> ans;
+        int psuedo = -1;
+
+        for(int i =0;i<nums.size();i++){
+            if(st.empty()) ans.push_back(psuedo);
+
+            else if(!st.empty() && st.top().first < nums[i]) ans.push_back(st.top().second);
+            else if(!st.empty() && st.top().first >= nums[i]){ 
+                while(!st.empty() && st.top().first >= nums[i]){
+                    st.pop();
+                }
+                if(st.size()==0) ans.push_back(psuedo);
+                else ans.push_back(st.top().second);
+            }
+            st.push({nums[i],i});
+        }
+        return ans;
+
+    }
+    vector<int> findRight (vector<int>& nums){
+        stack<pair<int,int>> st;
+        vector<int> ans;
+        int psuedo = nums.size();
+
+        for(int i =nums.size()-1;i>=0;i--){
+            if(st.empty()) ans.push_back(psuedo);
+
+            else if(!st.empty() && st.top().first < nums[i]) ans.push_back(st.top().second);
+            else if(!st.empty() && st.top().first >= nums[i]){ 
+                while(!st.empty() && st.top().first >= nums[i]){
+                    st.pop();
+                }
+                if(st.size()==0) ans.push_back(psuedo);
+                else ans.push_back(st.top().second);
+            }
+            st.push({nums[i],i});
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+
+    }
 public:
     int largestRectangleArea(vector<int>& heights) {
-        //left smaller index
 
-        stack<pair<int ,int>> s;
-        vector<int> left;
-        int psudoIndex = -1 ;
+        vector<int> left = findLeft(heights);
+        vector<int> right = findRight(heights);
+
+        vector<int> ans;
 
         for(int i =0;i<heights.size();i++){
-            if(s.size()==0) {
-                left.push_back(psudoIndex);
-            }
-            else if(s.size()>0 && s.top().first < heights[i]){
-                left.push_back(s.top().second);
-            }
-            else if(s.size()>0 && s.top().first >=heights[i]){
-                while(s.size()>0 && s.top().first >=heights[i]){
-                    s.pop();
-                }
-
-                    if(s.size()==0){
-                        left.push_back(psudoIndex);
-                    }
-
-                    else{
-                        left.push_back(s.top().second);
-                    }
-                
-            }
-
-            s.push({heights[i],i});
+            int x = (heights[i]) * (right[i]-left[i]-1);
+            ans.push_back(x);
         }
 
-        //right smaller index
-        while(s.size()>0){
-            s.pop();
-        }
-        
-        psudoIndex = heights.size();
-
-        vector<int> right;
-        
-
-        for(int i = heights.size()-1; i>=0 ;i--){
-            if(s.size()==0) {
-                right.push_back(psudoIndex);
-            }
-            else if(s.size()>0 && s.top().first < heights[i]){
-                right.push_back(s.top().second);
-            }
-            else if(s.size()>0 && s.top().first >=heights[i]){
-                while(s.size()>0 && s.top().first >=heights[i]){
-                    s.pop();
-                }
-
-                    if(s.size()==0){
-                        right.push_back(psudoIndex);
-                    }
-
-                    else{
-                        right.push_back(s.top().second);
-                    }
-                
-            }
-
-            s.push({heights[i],i});
-        }
-            reverse(right.begin(),right.end());
-
-            
-
-            vector<int> ans;
-            int area = 1;
-            for(int i =0;i<heights.size();i++){
-                area = heights[i] * (right[i]-left[i]-1);
-                ans.push_back(area);
-            }
-
-            
         return *max_element(ans.begin(),ans.end());
+        
     }
 };
