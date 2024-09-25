26b4e661a4819a59ffa6f608968ed349a1ee0e89
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2024-09-25 11:39:33 +0530

    Time: 15 ms (54.57%) | Memory: 12.8 MB (45.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,23 +3,25 @@ public:
     int lengthOfLongestSubstring(string s) {
         int left =0;
         int right =0;
+        int maxi=0;
+        map<char,int> mpp;
+
         int n = s.size();
-        int maxLen =0;
-        if(n==0) return 0;
-        map<char , int > mpp;
-        
 
         while(right<n){
             if(mpp.find(s[right])!=mpp.end()){
-                if(mpp[s[right]]>= left){
-                    left = mpp[s[right]]+1;
+                if(mpp[s[right]]>=left){
+                    left= mpp[s[right]]+1;
                 }
             }
+
             int len = right-left+1;
-            maxLen = max(maxLen , len);
-            mpp[s[right]] = right; //adding to hashmap
+            maxi = max(maxi , len);
+
+            mpp[s[right]]= right;
             right++;
         }
-        return maxLen;
+
+        return maxi;
     }
 };
