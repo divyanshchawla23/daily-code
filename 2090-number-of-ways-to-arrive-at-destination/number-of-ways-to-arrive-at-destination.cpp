59c0d96b5cbab9be0b7e78a623059ae87268a30d
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-05-16 23:45:55 +0530

    Time: 11 ms (36.09%) | Memory: 37.2 MB (32.81%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    int mod  = 1e9+7;
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        
+        vector<vector<pair<int,int>>> adj(n);
+
+        // [] -> {node,dis}
+        for(auto it: roads){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
+        }
+
+        vector<long long> dis(n,LLONG_MAX);
+        vector<int> vis(n,0);
+
+        dis[0]= 0;
+        vis[0]= 1;
+
+
+        // {dis,node}
+        priority_queue< pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>    >   pq;
+
+        pq.push({0,0});
+
+        while(!pq.empty()){
+            long long node= pq.top().second;
+            long long d = pq.top().first;
+            pq.pop();
+
+            for(auto it: adj[node]){
+                long long sum = (d+ it.second);
+                if(sum<dis[it.first]){
+                    dis[it.first]= sum;
+                    pq.push({sum,it.first});
+                    vis[it.first] = vis[node];
+                }
+                else if(sum ==dis[it.first]){
+                   
+                    vis[it.first] = (vis[it.first] + vis[node]) % mod;
+                }
+            }
+        }
+
+        return (vis[n-1] % mod);
+
+
+    }
+};
