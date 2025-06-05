f44d7af6d3cd93e007f5b347c9e44226429b9cca
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-06-05 18:27:16 +0530

    Time: 1 ms (52.32%) | Memory: 9 MB (47.31%) - LeetSync

---
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,57 @@
+class DSU{
+    public:
+    vector<int> parent;
+    vector<int> size;
+
+    DSU(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+
+    int findUParent(int v){
+        if(parent[v]==v) return v;
+
+        return parent[v] = findUParent(parent[v]);
+    }
+
+    void unionBySize(int u, int v){
+        int pu = findUParent(u);
+        int pv = findUParent(v);
+
+        if (pu == pv) return;
+
+        // Always attach larger to smaller
+        if (pu < pv) {
+            parent[pv] = pu;
+        } else {
+            parent[pu] = pv;
+        }
+    } 
+
+};
+class Solution {
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        int i =0;
+        int n = s1.size();
+        DSU ds(25);
+
+        while(i<n){
+           ds.unionBySize(s1[i]-'a',s2[i]-'a');
+           i++; 
+        }
+
+        string result = "";
+        for (char ch : baseStr) {
+            int smallest_equiv = ds.findUParent(ch - 'a');
+            result += (char)(smallest_equiv + 'a');
+        }
+
+        return result;
+
+    
+    }
+};
