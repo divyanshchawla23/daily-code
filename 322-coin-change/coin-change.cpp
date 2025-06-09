c0b1023f9707450e49e7cab97c9e9037effd8474
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-06-09 21:48:37 +0530

    Time: 55 ms (19.35%) | Memory: 48.7 MB (42.60%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,30 @@
 class Solution {
-private:
-    int f(int n, int amount, vector<int> &coins,vector<vector<int>> &dp){
-        if(amount == 0) return 0;
-
-        if(n==0){
-            if(amount%coins[n] == 0) return amount/coins[0];
-            else return 1e9;
-        }
-
-        if(dp[n][amount]!=-1) return dp[n][amount];
-
-        int notTake = 0 + f(n-1,amount,coins,dp);
-        int take = INT_MAX;
-        if(amount>=coins[n]) take = 1 + f(n,amount-coins[n],coins,dp);
-
-        return dp[n][amount] = min(notTake,take);
-
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans = f(n - 1, amount, coins,dp);  // FIXED: using correct index
-        return (ans >= 1e9) ? -1 : ans;
-        
+        vector<vector<int>> dp(n,vector<int>(amount+1,1e9));
+        for(int i =0;i<n;i++){
+            dp[i][0]=0;
+        }
+        for(int i =0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i] = i/coins[0];
+            }else dp[0][i] = 1e9;
+            
+        }  
+
+        for(int i =1;i<n;i++){
+            for(int j = 1;j<=amount;j++){
+                int notTake = 0 + dp[i-1][j];
+                int take = 1e9;
+                if(j>=coins[i]) take = 1 + dp[i][j-coins[i]];
+                dp[i][j] = min(notTake,take);
+
+            }
+        } 
+
+        int ans = dp[n-1][amount];
+
+        return (ans>=1e9) ? -1 : ans;
     }
 };
