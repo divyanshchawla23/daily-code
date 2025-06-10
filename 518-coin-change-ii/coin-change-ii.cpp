eaadd4e82d4d34b79c63af9363326894f40efbb6
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-06-10 22:11:59 +0530

    Time: 19 ms (55.25%) | Memory: 28 MB (67.08%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,28 +1,27 @@
 class Solution {
-private:
-    int f(int n, int target, vector<int>&coins,vector<vector<int>> &dp){
-        //base case
-        if(target==0) return 1;
-
-        if(n==0){
-            if(target%coins[0]==0) return 1;
-            else return 0;
-        } 
-
-        if(dp[n][target]!=-1) return dp[n][target];
-
-        long notPick = f(n-1,target,coins,dp);
-
-        long pick =0;
-        if(target>=coins[n]) pick = f(n,target-coins[n],coins,dp);
-
-        return dp[n][target] = pick+notPick;
-    }
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
 
-        return f(n-1,amount,coins,dp);
+        for(int i =0;i<n;i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i =0;i<=amount;i++){
+            dp[0][i] = (i%coins[0]==0);
+        }
+
+        for(int i =1;i<n;i++){
+            for(int j = 1 ;j<=amount;j++){
+                long notPick = dp[i-1][j];
+
+                long pick =0;
+                if(j>=coins[i]) pick = dp[i][j-coins[i]];
+                dp[i][j] = pick+notPick;
+            }
+        }
+
+        return dp[n-1][amount];
     }
 };
