cd38c7cf665582f57e988ca658bbb1324e2c39ce
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-06-10 23:30:27 +0530

    Time: 11 ms (43.94%) | Memory: 38 MB (26.67%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,84 +1,51 @@
 class Solution {
+
 private:
-    void bfs(int row, int col, vector<vector<int>>& grid,
-             vector<vector<int>>& vis, int drow[], int dcol[]) {
-                
-                int n = grid.size();
-                int m = grid[0].size();
-                
-                queue<pair<int,int>> q;
+    void dfs(int row, int col, vector<vector<int>>& grid,vector<vector<int>>& vis, int drow[],int dcol[],int n , int m){
 
-                q.push({row,col});
-                vis[row][col]=1;
+        vis[row][col]=1;
 
-                while(!q.empty()){
-                    int row = q.front().first;
-                    int col = q.front().second;
-                    q.pop();
+        for(int i =0;i<4;i++){
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
 
-                    for(int i =0;i<4;i++){
-                        int nrow = row + drow[i];
-                        int ncol = col + dcol[i];
-
-                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
-                        && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                            q.push({nrow,ncol});
-                             vis[nrow][ncol] = 1;
-                        }
-                    }
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                if(!vis[nrow][ncol] && grid[nrow][ncol]==1){
+                    dfs(nrow,ncol,grid,vis,drow,dcol,n,m);
                 }
+            }
+        }
 
-
-             }
-
+    }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int drow[4] = {-1, 0, 1, 0};
-        int dcol[4] = {0, 1, 0, -1};
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int drow[4] = {-1,0,1,0};
+        int dcol[4] = {0,+1,0,-1};
 
-        // check all the boundries;
-
-        // first row;
-
-        for (int i = 0; i < m; i++) {
-            if (grid[0][i] == 1 && vis[0][i] == 0) {
-                bfs(0, i, grid, vis, drow, dcol);
-            }
+        for(int i =0;i<n;i++){
+            if(grid[i][0]==1 && !vis[i][0]) dfs(i,0,grid,vis,drow,dcol,n,m);
         }
 
-        // last row;
-
-        for (int i = 0; i < m; i++) {
-            if (grid[n - 1][i] == 1 && vis[n - 1][i] == 0) {
-                bfs(n - 1, i, grid, vis, drow, dcol);
-            }
+        for(int i =0;i<n;i++){
+            if(grid[i][m-1]==1 && !vis[i][m-1]) dfs(i,m-1,grid,vis,drow,dcol,n,m);
         }
 
-        // first col;
-
-        for (int i = 0; i < n; i++) {
-            if (grid[i][0] == 1 && vis[i][0] == 0) {
-                bfs(i, 0, grid, vis, drow, dcol);
-            }
+        for(int j =0;j<m;j++){
+            if(grid[0][j]==1 && !vis[0][j]) dfs(0,j,grid,vis,drow,dcol,n,m);
         }
 
-        // last col;
-
-        for (int i = 0; i < n; i++) {
-            if (grid[i][m - 1] == 1 && vis[i][m - 1] == 0) {
-                bfs(i, m - 1, grid, vis, drow, dcol);
-            }
+        for(int j =0;j<m;j++){
+            if(grid[n-1][j]==1 && !vis[n-1][j]) dfs(n-1,j,grid,vis,drow,dcol,n,m);
         }
 
-        // check if there is 1 who is not vis add it to cnt;
         int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (vis[i][j] == 0 && grid[i][j] == 1) {
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]==1){
                     cnt++;
                 }
             }
