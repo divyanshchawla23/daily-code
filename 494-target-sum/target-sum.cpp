1af69f8106cf955e42d4c2ed1290f1cd7686c497
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-06-10 20:41:34 +0530

    Time: 171 ms (35.58%) | Memory: 11.5 MB (91.37%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,48 +1,32 @@
 class Solution {
-    int dp[21][1001];
-public:
-    int  helper(vector<int> nums , int k , int n){
-
-    for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= k; j++) {
-			if (i == 0) 
-				dp[i][j] = 0;
-			if (j == 0) 
-				dp[i][j] = 1;
-		}
-	}
-	
-    
-    for (int i = 1; i < n+1; i++)
-    {
-        for (int j = 0; j < k+1; j++)
-        {
-            if (nums[i - 1] <= j) {
-              dp[i][j] = (dp[i - 1][j - nums[i - 1]] + dp[i - 1][j]);
-            }
-
-            else
-              dp[i][j] = dp[i - 1][j];
+private:
+    int f(int n, int target , vector<int>& nums){
+        
+        if(n==0){
+            if(target==0 && nums[0]==0) return 2;
+            if(target==0 || target==nums[0]) return 1;
+            return 0;
         }
-    }
 
-    return dp[n][k];
-}
-    int findTargetSumWays(vector<int>& nums, int k) {
+        int notPick = f(n-1,target,nums);
+        int pick = 0;
+        if(target>=nums[n]) pick = f(n-1,target-nums[n],nums);
+
+        return pick+notPick;
+    }
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totalSum = 0;
+        for(auto it: nums){
+            totalSum+=it;
+        }
+        int res = (totalSum-target);
+
+        if(res % 2 != 0 || res < 0){
+            return 0;
+        }
         int n = nums.size();
 
-        int sumOfArray = 0;
-	    for (int i = 0; i < n; i++)
-        {    
-		sumOfArray += nums[i];  
-        }
-        int res =0;
-
-        int sum = (k+sumOfArray);
-        if (sum % 2 != 0 || sum < 0) return 0;
-
-        // Find the subset sum
-        return helper(nums, sum / 2, n);
-
+        return f(n-1,res/2,nums);
     }
 };
