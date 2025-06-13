3210ff429b31eda8c8b6f3fa709d752376fda372
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-06-14 00:27:10 +0530

    Time: 111 ms (5.76%) | Memory: 76 MB (21.48%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,32 +1,44 @@
 class Solution {
-public:
-
-    int dp[1001][1001];
-    int helper (string s1 , string s2, int n ,int m){
-        for(int i =0 ;i<=n;i++){
-            for(int j =0;j<=m;j++){
-                if(i==0||j==0) dp[i][j]=0;
-            }
-        }
-
-        for(int i =1 ;i<=n;i++){
-            for(int j =1;j<=m;j++){
-                if(s1[i-1]==s2[j-1])  dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j] = max (dp[i-1][j] , dp[i][j-1]);
-            }
-        }
-
-        return dp[n][m];
+private:
+    int longestCommonSubsequence(string text1, string text2,int n, int m)
+    {
         
-       
+        vector<vector<int>> dp(n, vector<int>(m, 0));
 
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (text1[i] == text2[j])
+                {
+                    dp[i][j] = 1;
+                    if (i > 0 && j > 0)
+                        dp[i][j] += dp[i - 1][j - 1];
+                    ;
+                }
+                else
+                {
+                    dp[i][j] = 0;
+
+                    int a = 0;
+                    int b = 0;
+                    if (i > 0)
+                        a = dp[i - 1][j];
+                    if (j > 0)
+                        b = dp[i][j - 1];
+
+                    dp[i][j] += max(a, b);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
+public:
     int longestPalindromeSubseq(string s) {
-      
-      int n = s.size();  
-      string s1 = s; 
-      reverse(s.begin(),s.end());
-      int m = s1.size();
-      return  helper(s,s1,n,m);
+        string s1 = s;
+        reverse(s1.begin(),s1.end());
+        int n = s.size();
+        int m = s1.size();
+        return longestCommonSubsequence(s,s1,n,m);
     }
 };
