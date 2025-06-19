c72bf082ce8b9fbba91dd5304a41357cb5f54c7e
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-06-19 21:41:09 +0530

    Time: 23 ms (88.76%) | Memory: 26.6 MB (95.25%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -4,62 +4,52 @@ public:
         int n = str1.size();
         int m = str2.size();
 
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (str1[i] == str2[j]) {
+        for(int i =0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0||j==0) dp[i][j]=0;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
                     dp[i][j] = 1;
                     if (i > 0 && j > 0)
                         dp[i][j] += dp[i - 1][j - 1];
                 } else {
-                    int a = (i > 0) ? dp[i - 1][j] : 0;
-                    int b = (j > 0) ? dp[i][j - 1] : 0;
+                    int a = dp[i - 1][j];
+                    int b =  dp[i][j - 1];
                     dp[i][j] = max(a, b);
                 }
             }
         }
 
         string ans = "";
-        int i = n - 1, j = m - 1;
+        int i = n, j = m;
 
-        while (i >= 0 && j >= 0) {
-        if (str1[i] == str2[j]) {
-            ans += str1[i];
-            i--;
-            j--;
-        } else if (i == 0) {
-            ans += str2[j];
-            j--;
-        } else if (j == 0) {
-            ans += str1[i];
-            i--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            ans += str1[i];
-            i--;
-        } else {
-            ans += str2[j];
-            j--;
+        while (i > 0 && j > 0) {
+            if (str1[i-1] == str2[j-1]) {
+                ans += str1[i-1];
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                ans += str1[i-1];
+                i--;
+            } else {
+                ans += str2[j-1];
+                j--;
+            }
         }
-    }
 
-    while (i >= 0) {
-        ans += str1[i];
-        i--;
-    }
-
-    while (j >= 0) {
-        ans += str2[j];
-        j--;
-    }
-
-        while (i >= 0) {
-            ans += str1[i];
+        while (i > 0) {
+            ans += str1[i-1];
             i--;
         }
 
-        while (j >= 0) {
-            ans += str2[j];
+        while (j > 0) {
+            ans += str2[j-1];
             j--;
         }
 
