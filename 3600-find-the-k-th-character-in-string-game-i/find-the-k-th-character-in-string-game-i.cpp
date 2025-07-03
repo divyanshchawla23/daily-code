3d44c0d0a6a64a81f2253b5175cca40dad5663bc
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-07-03 14:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (80.06%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string s = "a";
+
+        while(s.size()<k){
+            int n = s.size();
+            for(int i =0;i<n;i++){
+                if(s[i]=='z'){
+                    s+='a';
+                }else{
+                    s+=s[i]+1;
+                }
+            }
+        }
+        return s[k-1];
+    }
+};
