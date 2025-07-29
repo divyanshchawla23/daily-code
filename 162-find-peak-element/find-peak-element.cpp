2212165575330ca8317bc656b341b69cc2b7d601
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-07-29 21:59:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (32.17%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         if(n==1) return 0;
         int start = 1 ;
-        int end = n-1;
+        int end = n-2;
 
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
