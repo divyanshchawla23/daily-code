05dcde77fff62756648b0aa264bf01fc720974f2
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-07-30 12:17:14 +0530

    Time: 45 ms (73.91%) | Memory: 61.6 MB (65.79%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    bool canWePlace(vector<int>& position, int mid , int cows){
-        int cntCow =1;
-        int last = position[0];
+    bool canWePlace(vector<int>& nums, int mid, int m){
+        int lastPlace = nums[0];
+        int cowsCnt =1;
 
-        for(int i =1;i<position.size();i++){
-            if(position[i]-last>= mid){
-                cntCow ++;
-                last = position[i];
+        for(int i =1;i<nums.size();i++){
+            if(nums[i]-lastPlace>=mid){
+                cowsCnt += 1;
+                lastPlace = nums[i];
             }
-            if(cntCow>=cows) return true;
-        }
-             return false ;
 
+        }
+        if(cowsCnt>=m) return true;
+        else return false;
     }
-    
     int maxDistance(vector<int>& position, int m) {
-
         sort(position.begin(),position.end());
-
-        int low = 1;
-        int high = *max_element(position.begin(),position.end());
-
+        int maxDist = *max_element(position.begin(),position.end()) 
+        - *min_element(position.begin(),position.end());
         int ans = INT_MIN;
+        int n = position.size();
+        int l =1;
+        int r = maxDist;
 
-        while(low<=high){
-
-            int mid = (low +high)/2;
-
-            if(canWePlace(position, mid , m)== true){
-                ans = mid ;
-               low = mid +1; 
+        while(l<=r){
+            int mid = (l+r)/2;
+            
+            if(canWePlace(position,mid,m)){
+                ans = mid;
+                l = mid+1;
             }
-
-            else{
-                high = mid -1 ;
-            }
+            else r = mid-1;
+            
         }
-        return ans ;
-        
+        return ans;
     }
 };
