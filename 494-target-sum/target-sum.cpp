acf771e2f89d6fd7e299966fd2fd209c36b7599e
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-08-01 00:07:12 +0530

    Time: 4 ms (79.04%) | Memory: 15.4 MB (52.18%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,32 +1,40 @@
 class Solution {
-private:
-    int f(int n, int target , vector<int>& nums){
-        
-        if(n==0){
-            if(target==0 && nums[0]==0) return 2;
-            if(target==0 || target==nums[0]) return 1;
-            return 0;
-        }
-
-        int notPick = f(n-1,target,nums);
-        int pick = 0;
-        if(target>=nums[n]) pick = f(n-1,target-nums[n],nums);
-
-        return pick+notPick;
-    }
 public:
+    int f(int ind, vector<int>& arr, int target, vector<vector<int>>& dp) {
+        if (target == 0) return 1;
+        if (ind == 0) return (arr[0] == target);
+
+        if (dp[ind][target] != -1) return dp[ind][target];
+
+        int notPick = f(ind - 1, arr, target, dp);
+        int pick = 0;
+        if (arr[ind] <= target)
+            pick = f(ind - 1, arr, target - arr[ind], dp);
+
+        return dp[ind][target] = pick + notPick;
+    }
+
+    int findWays(vector<int>& arr, int target) {
+        int cnt = 0;
+        vector<int> newArr;
+        for (int x : arr) {
+            if (x == 0) cnt++;      // count zeros
+            else newArr.push_back(x); // keep non-zeros
+        }
+
+        int n = newArr.size();
+        if (target < 0) return 0;
+        if (n == 0) return (target == 0) ? pow(2, cnt) : 0;
+
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        int ways = f(n - 1, newArr, target, dp);
+        return ways * pow(2, cnt);  // adjust for zeros
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalSum = 0;
-        for(auto it: nums){
-            totalSum+=it;
-        }
-        int res = (totalSum-target);
-
-        if(res % 2 != 0 || res < 0){
-            return 0;
-        }
-        int n = nums.size();
-
-        return f(n-1,res/2,nums);
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        int x = totalSum - target;
+        if (x < 0 || x % 2 != 0) return 0;
+        return findWays(nums, x / 2);
     }
 };
