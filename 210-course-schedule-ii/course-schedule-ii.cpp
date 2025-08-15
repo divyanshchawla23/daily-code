447fcef22bd14c51a45d1eca06b5b26abeb52f11
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-08-15 23:03:26 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (35.83%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,53 +1,40 @@
 class Solution {
 public:
-    bool checkCycle(int node, int parent, vector<vector<int>>& nums,
-                    vector<vector<int>>& adj, vector<int>& vis,
-                    vector<int>& pvis,vector<int>&topo,stack<int>& st) {
-
-        vis[node] = 1;
-        pvis[node] = 1;
-
-        for (auto it : adj[node]) {
-            if (!vis[it]) {
-                if (checkCycle(it, node, nums, adj, vis, pvis,topo,st))
-                    return true;
-            } else if (pvis[it]) {
-                return true;
-            }
-        }
-        st.push(node);
-        pvis[node] = 0;
-        return false;
-    }
-
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites,vector<int>&topo,stack<int>&st) {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
         vector<vector<int>> adj(n);
-        for (auto& it : prerequisites) {
+        
+        for(auto it: prerequisites){
             adj[it[1]].push_back(it[0]);
         }
 
-        vector<int> vis(n, 0);
-        vector<int> pvis(n, 0);
+        queue<int>q;
+        vector<int> inDegree(n,0);
 
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                if (checkCycle(i, -1, prerequisites, adj, vis, pvis,topo,st))
-                    return false;
+        for(int i =0;i<n;i++){
+            for(auto it: adj[i]){
+                inDegree[it]++;
             }
         }
-        return true;
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        stack<int>st;
-        vector<int> topo;
-        bool ans = canFinish(numCourses,prerequisites,topo,st);
-        while(!st.empty()){
-            int node =st.top();
-            st.pop();
-            topo.push_back(node);
+
+        for(int i =0;i<n;i++){
+            if(inDegree[i]==0){
+                q.push(i);
+            }
         }
-        if(ans==false) return {};
-        else return topo;
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size()==n) return topo;
+        else return {};
     }
 };
