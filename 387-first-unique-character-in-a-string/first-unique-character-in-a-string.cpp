3c88c6b33c127d66701e85fc74d39f7ad721c65d
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-08-25 00:20:43 +0530

    Time: 22 ms (21.33%) | Memory: 14.9 MB (43.81%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<int,int> mpp;
+
+        for(char c: s){
+            mpp[c]++;
+        }
+
+        for(int i =0;i<s.size();i++){
+            if(mpp[s[i]]==1) return i;
+        }
+
+        return -1;
+    }
+};
