1f32e34c3cd14990c0b8b3b3700a17fcc5dce16a
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-08-26 15:36:39 +0530

    Time: 671 ms (19.67%) | Memory: 295.8 MB (7.14%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,58 +1,48 @@
 class Solution {
 public:
+    void sorting(vector<int>&nums,int low,int mid,int high){
+        int left = low;
+        int right = mid+1;
 
-    void merge(vector<int>& arr, int n, int low, int mid, int high)
-{
-    vector<int> ans;
-    int left = low;
-    int right = mid + 1;
+        vector<int> ans;
 
-    while (left <= mid && right <= high)
-    {
-        if (arr[left] <= arr[right])
-        {
-            ans.push_back(arr[left]);
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]){
+                ans.push_back(nums[left]);
+                left++;
+            }else{
+                ans.push_back(nums[right]);
+                right++;
+            }
+        } 
+
+        while(left<=mid){
+            ans.push_back(nums[left]);
             left++;
         }
-        else
-        {
-            ans.push_back(arr[right]);
+
+        while(right<=high){
+            ans.push_back(nums[right]);
             right++;
         }
-    }
 
-    while (left <= mid)
-    {
-        ans.push_back(arr[left]);
-        left++;
+        for(int i =low;i<=high;i++){
+            nums[i]= ans[i-low];
+        }
     }
-
-    while (right <= high)
-    {
-        ans.push_back(arr[right]);
-        right++;
+    void helper(vector<int>&nums, int low , int high){
+        if(low<high){
+            int mid = (low+high)/2;
+            helper(nums,low,mid);
+            helper(nums,mid+1,high);
+            sorting(nums,low,mid,high);
+        }
     }
-
-    for (int i = low; i <= high; i++)
-    {
-        arr[i] = ans[i - low];
-    }
-}
-
-void merge_sort(vector<int>& arr, int n, int low, int high)
-{
-    if (low >= high)
-    {
-        return;
-    }
-    int mid = (low + high) / 2;
-    merge_sort(arr, n, low, mid);
-    merge_sort(arr, n, mid + 1, high);
-    merge(arr, n, low, mid, high);
-}
-
-    vector<int> sortArray(vector<int>& arr) {
-        merge_sort(arr,arr.size(),0,arr.size()-1);
-        return arr;
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        helper(nums,low,high);
+        return nums;
     }
 };
