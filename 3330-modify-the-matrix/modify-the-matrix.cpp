c8760611de87b004d77e22dadd9e9fc8f045719a
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-09-06 22:52:16 +0530

    Time: 2 ms (30.77%) | Memory: 23.6 MB (34.72%) - LeetSync

---
+++ 3330-modify-the-matrix/modify-the-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> maxEl(m, INT_MIN);
+
+        for (int j = 0; j < m; j++) {
+            int maxi = INT_MIN;
+            for (int i = 0; i < n; i++) {
+                maxi = max(maxi, matrix[i][j]);
+            }
+            maxEl[j] = maxi;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = maxEl[j];
+                }
+            }
+        }
+
+        return matrix;
+    }
+};
