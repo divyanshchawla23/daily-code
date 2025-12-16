6b79db33f44fbec788685d2902de13a899169149
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2025-12-16 19:36:50 +0530

    Time: 7 ms (27.37%) | Memory: 28.7 MB (37.01%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int dfs(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& vis,int n,int m,int drow[],int dcol[]){
+        vis[i][j]=1;
+
+        int size = 1;
+
+        for(int ind = 0;ind<4;ind++){
+            int nrow = i + drow[ind];
+            int ncol = j + dcol[ind];
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                if(grid[nrow][ncol]==1 && !vis[nrow][ncol]){
+                    size += dfs(nrow,ncol,grid,vis,n,m,drow,dcol);
+                }
+            }
+        }
+
+        return size;    
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int drow[4] = {-1,0,1,0};
+        int dcol[4] = {0,1,0,-1};
+        int n = grid.size();
+        int m = grid[0].size();
+        int maxi = 0;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j]==1){
+                    int size = dfs(i,j,grid,vis,n,m,drow,dcol);
+                    maxi = max(maxi,size);
+                }
+            }
+        }
+        return maxi;
+    }
+};
