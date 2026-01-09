934e335bf6ba3821ec889f159fecb9e45938fb83
Divyansh Chawla <149439705+divyanshchawla23@users.noreply.github.com>
2026-01-09 22:44:09 +0530

    Time: 2 ms (23.38%) | Memory: 24.9 MB (6.17%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void levelOrder(TreeNode* root, vector<vector<TreeNode*>>& levels,
+                    map<TreeNode*, TreeNode*>& mpp) {
+
+        queue<TreeNode*> q;
+        q.push(root);
+        mpp[root] = NULL;
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<TreeNode*> level;
+
+            for (int i = 1; i <= size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node);
+
+                if (node->left) {
+                    q.push(node->left);
+                    mpp[node->left] = node;
+                }
+                if (node->right) {
+                    q.push(node->right);
+                    mpp[node->right] = node;
+                }
+            }
+
+            levels.push_back(level);
+        }
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        vector<vector<TreeNode*>> levels;
+        map<TreeNode*, TreeNode*> mpp; // node -> parent
+        levelOrder(root, levels, mpp);
+
+        vector<TreeNode*> last = levels.back();
+        set<TreeNode*> st(last.begin(), last.end());
+
+        while (st.size() > 1) {
+            set<TreeNode*> s;
+            for (auto& it : st) {
+                s.insert(mpp[it]);
+            }
+            st = move(s);
+        }
+
+        return *st.begin();
+    }
+};
